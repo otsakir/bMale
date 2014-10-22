fc8d5f376fa993990c6d2d46eab911f3ee81ec25
otsakiridis <orestis.tsakiridis@telestax.com>
2014-10-22 13:21:08 +0300

    Fixes #3 Added a workaroung function for extracting bmaleticket cookie. New bmale_utils module to host general-purpose utility functions

---
+++ bmale_utils.lua
@@ -0,0 +1,11 @@
+local module = {}
+
+-- a workaround to extract the bmaleticket from the header until we sort why orbit's web.cookies table does not work
+-- header can be 'web.vars.HTTP_COOKIE'
+-- returns the cookie's content or nil if no such cookie is found
+function module.extractTicketFromHeader( header )
+	return header:match("bmaleticket=([^;]+);")
+end
+
+
+return module
